5b2639d59daed4fc81fe84cddff3c419cf4f0a77
IgorRyabenko <riabenko.igor@gmail.com>
2022-07-13 22:27:49 +0300

    added selectors

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,26 +5,21 @@ import s from './users.module.css';
 // import { connect } from 'react-redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { filterUser, removeUser } from 'redux/actions';
+import { getfilteredUsers } from 'redux/selectors';
 
-const filteredUsers = (allUsers, filter) => {
-  const normalizedFilter = filter.toLowerCase();
-
-  return allUsers.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter)
-  );
-};
 
 // const Users = ({ removeUser, getFilteredUsers, handleFilter }) => {
 const Users = () => {
-  const items = useSelector(({ contacts: { items, filter } }) =>
-    filteredUsers(items, filter)
-  );
+  // const items = useSelector(({ contacts: { items, filter } }) =>
+  //   filteredUsers(items, filter)
+  // );
+  const users = useSelector(getfilteredUsers)
   const dispatch = useDispatch();
 
   const onRemoveUser = id => dispatch(removeUser(id));
   const onHandleFilter = e => dispatch(filterUser(e.target.value));
 
-  const elements = items.map(({ id, name, number }) => (
+  const elements = users.map(({ id, name, number }) => (
     <li className={s.item} key={id}>
       {name}: {number}{' '}
       <button className={s.btn} onClick={() => onRemoveUser(id)}>
