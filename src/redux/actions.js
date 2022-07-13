a0c00ed42e262e977d8e361d531b00294e4ed34a
IgorRyabenko <riabenko.igor@gmail.com>
2022-07-13 15:15:54 +0300

    Redux toolkit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,23 +1,35 @@
-import { nanoid } from "nanoid";
-import { ADD_USER, REMOVE_USER, FILTER_USER } from "./types";
+// import { ADD_USER, REMOVE_USER, FILTER_USER } from './types';
+import { nanoid } from 'nanoid';
+import { createAction } from '@reduxjs/toolkit';
 
-export const addUser = (payload) => {
-  return {
-    type: ADD_USER,
-    payload: {...payload, id: nanoid()},
-   }
-}
+export const addUser = createAction('user/add', user => ({
+  payload: {
+    ...user,
+    id: nanoid(),
+  },
+}));
 
-export const removeUser = (id) => {
-  return {
-    type: REMOVE_USER,
-    payload: id
-  }
-}
+// export const addUser = (payload) => {
+//   return {
+//     type: ADD_USER,
+//     payload: {...payload, id: nanoid()},
+//    }
+// }
 
-export const filterUser = value => {
-  return {
-    type: FILTER_USER,
-    payload: value
-  }
-}
+export const removeUser = createAction('user/remove');
+
+// export const removeUser = (id) => {
+//   return {
+//     type: REMOVE_USER,
+//     payload: id
+//   }
+// }
+
+export const filterUser = createAction('user/filter');
+
+// export const filterUser = value => {
+//   return {
+//     type: FILTER_USER,
+//     payload: value
+//   }
+// }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,37 +1,50 @@
-import { ADD_USER, REMOVE_USER, FILTER_USER } from './types';
+// import { ADD_USER, REMOVE_USER, FILTER_USER } from './types';
+import { addUser, removeUser, filterUser } from './actions';
 import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
 
-const itemsReducer = (state = [], { type, payload }) => {
-  switch (type) {
-    case ADD_USER:
-      return [...state, payload];
+const itemsReducer = createReducer([], {
+  [addUser]: (state, { payload }) => [...state, payload],
+  [removeUser]: (state, { payload }) =>
+    state.filter(({ id }) => id !== payload),
+});
 
-    case REMOVE_USER:
-      return state.filter(({id}) => id !== payload);
+// const itemsReducer = (state = [], { type, payload }) => {
+//   switch (type) {
+//     case ADD_USER:
+//       return [...state, payload];
 
-    default:
-      return state;
-  }
-};
+//     case REMOVE_USER:
+//       return state.filter(({id}) => id !== payload);
 
-const filterReducer = (state = '', { type, payload }) => {
-  switch(type) {
-    case FILTER_USER:
-      return payload;
+//     default:
+//       return state;
+//   }
+// };
 
-    default:
-      return state;
-  }
+const filterReducer = createReducer('', {
+  [filterUser]: (_, { payload }) => payload,
+});
 
-};
+// const filterReducer = (state = '', { type, payload }) => {
+//   switch (type) {
+//     case FILTER_USER:
+//       return payload;
+
+//     default:
+//       return state;
+//   }
+// };
 
 const usersReducer = combineReducers({
   items: itemsReducer,
   filter: filterReducer,
 });
 
-const rootReducer = combineReducers({
-  contacts: usersReducer,
-});
+// const rootReducer = combineReducers({
+//   contacts: usersReducer,
+// });
 
-export default rootReducer;
+export default usersReducer;
+
+// export default rootReducer;
